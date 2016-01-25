7cf86383423efb8b38c052985c1be8e6496804f6
Yana Kultysheva <ykultysheva@gmail.com>
2016-01-25 18:17:58 -0500

    Adding feature to delete picture

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "pictures#index"
   get "pictures" => "pictures#index"
 
-  get 'pictures' => 'pictures#index'
+  # get 'pictures' => 'pictures#index'
 
   post "pictures" => "pictures#create"
   get "pictures/new" => "pictures#new"
@@ -13,6 +13,20 @@ Rails.application.routes.draw do
 
 
   get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+
+
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+
+  # root 'pictures#index'
+
+
+
+
+
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
