661156c395ad038542e8f52c7d6960f72d83b5b7
Yana Kultysheva <ykultysheva@gmail.com>
2016-01-28 13:31:52 -0500

    Initial commit

--- photogur/db/schema.rb
+++ photogur/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125194749) do
+ActiveRecord::Schema.define(version: 20160128152438) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "message"
+    t.integer  "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
